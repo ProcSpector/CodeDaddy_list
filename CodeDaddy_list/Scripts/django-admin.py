59ffda6cb0b0b552e8c770a19ef66393804463ee
Aniketh Vankina <sbvankina@yahoo.com>
2020-05-04 22:09:46 -0700

    sample project

---
+++ CodeDaddy_list/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\av_a1\PycharmProjects\CodeDaddy_list\CodeDaddy_list\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
